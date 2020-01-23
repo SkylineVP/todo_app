d0d628b3f4a6b5f9c9d6edfdb13d6f66d47bd51e
SkylineVP <vladpolizp@gmail.com>
2020-01-23 16:48:32 +0200

    Bad initial commit

---
+++ server/src/db/config/config.js
@@ -0,0 +1,15 @@
+module.exports={
+    development:{
+        username: "skyline",
+        password: "skyline",
+        database: "fd_todo_db",
+        host: "127.0.0.1",
+        dialect: "postgres",
+    },
+    test:{
+
+    },
+    production:{
+
+    },
+};
---
+++ server/src/index.js
@@ -0,0 +1,96 @@
+import {User,sequelize} from './db/models';
+import {Model, Sequelize, DataType, DataTypes} from "sequelize";
+import moment from "moment";
+const users=[];
+class Task extends Model {}
+Task.init(
+    {
+        value: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        deadline:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            validate:{
+                isDate:true,
+                isAfter: moment().format('YYYY-MM-DD')
+            }
+        },
+        isDone:{
+            type:DataTypes.BOOLEAN,
+            defaultValue:false,
+            allowNull:false
+        }
+
+    },
+    {
+        timestamps: true,
+        sequelize
+    });
+Task.belongsTo(User);
+User.hasMany(Task);
+Task.sync();
+User.findByPk(46).then(data=>console.log(data.get()));
+User.findAll().then(data=>{
+    data.forEach(user=>{
+        users.push(user.get());
+    });
+    console.log(users);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createUser({firstName,lastName,login,password,email}){
+User.create()
+}
+
+async function getUser(id) {
+    try{
+        const user = await User.findByPk(id);
+         if (user)
+             return user.get();
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+async function updateUser(id,data){
+    try {
+        const oldUser=await User.findByPk(id);
+        const updatedUser= await oldUser.update(data);
+        return updatedUser.get();
+
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+/*
+getUser(2).then(console.log);
+
+updateUser(2,{
+    firstName:"Ronaldo"}).then(console.log);*/
